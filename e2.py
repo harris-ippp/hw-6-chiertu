c7cd0bc0ed36748653b9a6433ef3b9f6bffb168b
Yiran <haoyiran@uchicago.edu>
2017-11-07 16:18:58 -0600

    q2

---
+++ e2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import requests
+
+#download the data for every year in the ELECTION_ID list
+for line in open("ELECTION_ID"):
+    resp = requests.get("http://historical.elections.virginia.gov/elections/download/{}/precincts_include:0/".format(line.rpartition(' ')[2]))
+    file_name = line.partition(' ')[0] +".csv"
+    with open(file_name, "w") as out:
+        out.write(resp.text)
+
+#rename the data file for year 2016
+os.rename("2016.csv","president_general_2016.csv")
